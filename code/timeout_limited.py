d45aecd3961e410993bdd791387bda94b7c49c32
sktwj <409368545@qq.com>
2019-11-13 18:24:02 +0800

    add timeout limited py

---
+++ code/timeout_limited.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from threading import Thread
+import time
+
+import inspect
+import ctypes
+
+class TimeoutException(Exception):
+    pass
+
+ThreadStop = Thread._Thread__stop #获取私有函数
+
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid,
+                                                     ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
+
+def stop_thread(thread):
+    _async_raise(thread.ident, SystemExit)
+
+
+def timelimited(timeout):
+    def decorator(function):
+        def decorator2(*args,**kwargs):
+            class TimeLimited(Thread):
+                def __init__(self,_error= None):
+                    Thread.__init__(self)
+                    self._error =  _error
+
+                def run(self):
+                    try:
+                        self.result = function(*args,**kwargs)
+                    except Exception as e:
+                        self._error = e
+
+                def _stop(self):
+                    if self.isAlive():
+                        ThreadStop(self)
+
+            t = TimeLimited()
+            t.start()
+            t.join(timeout)
+
+            if isinstance(t._error,TimeoutException):
+                t._stop()
+                raise TimeoutException('timeout for %s' % (repr(function)))
+
+            if t.isAlive():
+                t._stop()
+                raise TimeoutException('timeout for %s' % (repr(function)))
+
+            if t._error is None:
+                return t.result
+
+        return decorator2
+    return decorator
+
+@timelimited(2)
+def dave(secs):
+    time.sleep(secs)
+    print "My Blog is: http://www.cndba.cn/dave"
+    return 'Finished'
+
+if __name__ == "__main__":
+    print dave(4)
